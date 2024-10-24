b1cd18efe413a2640196a494eaac0581a1ae6aab
J3fF1992 <jefferson.reis00@hotmail.com>
2024-10-24 16:52:15 -0300

    Created Router Atleta

---
+++ workout_api_dio/configs/settings.py
@@ -0,0 +1,9 @@
+from pydantic_settings import BaseSettings
+from pydantic import Field
+
+class Settings(BaseSettings):
+    DB_URL: str = Field(default='postgresql+asyncpg://user:123456@localhost/mydb')
+
+settings= Settings()
+
+                        
