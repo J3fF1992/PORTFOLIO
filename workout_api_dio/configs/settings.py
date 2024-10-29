8d9b41188b8d913b3edee02a4870a2891dc4cb02
J3fF1992 <jefferson.reis00@hotmail.com>
2024-10-28 23:39:43 -0300

    Agora vai

--- workout_api_dio/configs/settings.py
+++ workout_api_dio/configs/settings.py
@@ -1,9 +1,10 @@
 from pydantic_settings import BaseSettings
 from pydantic import Field
 
+
 class Settings(BaseSettings):
     DB_URL: str = Field(default='postgresql+asyncpg://user:123456@localhost/mydb')
 
 settings= Settings()
 
-                        
+#teste funcional
