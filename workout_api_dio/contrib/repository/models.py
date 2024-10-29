1aceec99dda66e4189e3880cef6d6e26d59b4ef8
J3fF1992 <jefferson.reis00@hotmail.com>
2024-10-29 10:34:40 -0300

    Start

--- workout_api_dio/contrib/repository/models.py
+++
@@ -1,3 +0,0 @@
-from workout_api_dio.categorias.models import CategoriaModel
-from workout_api_dio.atleta.models import AtletaModel
-from workout_api_dio.centro_treinamento.models import CentroTreinamentoModel
